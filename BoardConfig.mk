002698cd82347cc2fb4b71d84bcf7abdeac4fec8
ssch71 <s01093450498@gmail.com>
2026-04-05 17:12:32 +0900

    Add files via upload

---
+++ BoardConfig.mk
@@ -0,0 +1,30 @@
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+# Platform
+TARGET_BOARD_PLATFORM := exynos7870
+TARGET_SOC := exynos7870
+BOARD_VENDOR := samsung
+
+# Kernel Details
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.hardware=samsungexynos7870 loop.max_part=7
+BOARD_KERNEL_OFFSET := 0x00008000
+BOARD_RAMDISK_OFFSET := 0x01000000
+BOARD_TAGS_OFFSET := 0x00000100
+
+BOARD_MKBOOTIMG_ARGS := --base $(BOARD_KERNEL_BASE) --pagesize $(BOARD_KERNEL_PAGESIZE) --kernel_offset $(BOARD_KERNEL_OFFSET) --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_TAGS_OFFSET)
+
+# TWRP Specifics
+TW_THEME := landscape_hdpi
+RECOVERY_SDCARD_ON_DATA := true
+TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
+TW_BRIGHTNESS_PATH := "/sys/class/backlight/panel/brightness"
+TW_MAX_BRIGHTNESS := 255
+TW_DEFAULT_LANGUAGE := ko
+TW_INPUT_BLACKLIST := "hbtp_vm"
