7c35fb1f4d6f138d06398d02d46cb861222514ef
ssch71 <s01093450498@gmail.com>
2026-04-06 23:27:22 +0900

    Change product inheritance from wifionly to full
    Updated product inheritance for LTE devices.

--- lineage_gtanotexl.mk
+++ lineage_gtanotexl.mk
@@ -1,5 +1,5 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/lineage/config/common_full_tablet.mk)  # 수정: wifionly → full (LTE 기기)
 $(call inherit-product, device/samsung/gtanotexl/device.mk)
 
 PRODUCT_NAME := lineage_gtanotexl
